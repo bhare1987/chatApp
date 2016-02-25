564315d47aa4b5e7e9d719ce20a3fc24e460786d
bhare1987 <brandonlhare@gmail.com>
2016-02-25 13:48:05 -0500

    updated app.js

--- app.js
+++ app.js
@@ -6,25 +6,45 @@ var BrandTricks = {
   },
   config: {
     url: "http://tiny-tiny.herokuapp.com/collections/BrandTricks",
+    msgUrl: BrandTricks.config.url + "/" + "msg/"
+    userUrl: BrandTricks.config.url + "/" + "user/"
   },
   presentation: function() {
   },
   events: function(){},
   getMsg: function() {
     $.ajax({
-      url: BrandTricks.config.url,
+      url: BrandTricks.config.url + "/" + "msg",
       method: 'GET',
       success: function (messages) {
         console.log(messages);
-        myBlog.addAllPostsToDom(blogPosts);
       },
       error: function (err) {
         console.log(err);
       }
     });
   },
-  addMsg: function(messageObj) {},
-  deleteMsg: function(messageID) {},
+  addMsg: function(messageObj) {
+    $.ajax({
+      url: BrandTricks.config.msgUrl,
+      method: "POST",
+      data: messageObj,
+      success: function(messages){
+        BrandTricks.getMsg();
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  },
+  deleteMsg: function(messageID) {
+    $.ajax({
+      url: BrandTricks.config.userUrl,
+      method: 'DELETE',
+      success: function (response) {
+        myBlog.getPosts();
+    });
+  },
   getUser: function() {},
   addUser: function() {},
 
