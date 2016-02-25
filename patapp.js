09432041d6be28b908ffa985f6e0ec9436904993
Patrick Coleman <colemanrpatrick@gmail.com>
2016-02-25 14:57:03 -0500

    does this work?

--- patapp.js
+++ patapp.js
@@ -1,4 +1,37 @@
-function messageToPage(Data, tmplStr, $target) {
-    var tmpl = _.template(tmplStr);
-    $target.append(tmpl(Data));
+var messages ={
+  [        ]
 };
+
+function userInput(){
+  var contenT = $('input["name=chat"]').val();
+  return contenT;
+}
+
+function displayMessage(data, str, $target){
+  var messtmpl = _.template(str);
+  $target.append(messtmpl(data));
+};
+
+function getMessage(){
+  return messages;
+};
+
+function showNewMessage(messagetmpl){
+messages.push(messagetmpl)
+};
+
+function addGetMssg(){
+  $('section').html('');
+  _.each(), function (el,i) {
+  el.id = _id;
+  displayMessage(el, templates.messagetpl, $('section'));
+  }
+}
+
+$('form').on('submit', function(event){
+event.preventDefault();
+var NewMessage = userInput()
+showNewMessage(NewMessage);
+addGetMssg(getMessage());
+$('input').val('');
+});
---
+++ template.js
@@ -0,0 +1,9 @@
+var templates = {
+ messagetmpl: [
+   "<div data-id='<%= _id %>'>",
+     "<p><%= content %></p>",
+     "<span><%= name %></span>",
+     "<span><%= date %></span>",
+     "</div>",
+ ].join("")
+};
