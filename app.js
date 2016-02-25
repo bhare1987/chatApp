3a519471e1a645c3f42900090589758199c71b99
bhare1987 <brandonlhare@gmail.com>
2016-02-25 13:35:41 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-function messageToPage(Data, tmplStr, $target) {
-    var tmpl = _.template(tmplStr);
-    $target.append(tmpl(Data));
-};
+
+var BrandTricks = {
+  init: function() {
+    BrandTricks.presentation();
+    BrandTricks.events();
+  },
+  config: {
+    url: "http://tiny-tiny.herokuapp.com/collections/BrandTricks",
+  },
+  presentation: function() {
+  },
+  events: function(){},
+  getMsg: function() {
+    $.ajax({
+      url: BrandTricks.config.url,
+      method: 'GET',
+      success: function (messages) {
+        console.log(messages);
+        myBlog.addAllPostsToDom(blogPosts);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  },
+  addMsg: function(messageObj) {},
+  deleteMsg: function(messageID) {},
+  getUser: function() {},
+  addUser: function() {},
+
+}
+
+$(document).ready(function(){
+  BrandTricks.init();
+});
