50af685bc56efe0b9d4b6bc3e11ad64f063a457b
Patrick Coleman <colemanrpatrick@gmail.com>
2016-02-26 10:34:01 -0500

    user bar

--- template.js
+++ template.js
@@ -1,9 +1,15 @@
-var templates = {
- messagetmpl: [
+var templates = {};
+ templates.messagetmpl=[
    "<div data-id='<%=_id %>'>",
      "<span class='usenm'><%= username %></span>",
      "<span class='d8'><%= date %></span>",
      "<div class='messagediv'><%= content %>",
      "</div>",
- ].join("")
-};
+ ].join("");
+
+  templates.usertmpl= [
+    "<div data-id='<%=_id %>'>",
+    "<li>",
+    "<span class='usenm'><%= username %></span>",
+    "</li>",
+  ].join("");
