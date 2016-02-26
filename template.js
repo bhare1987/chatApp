5ead53c283ae2d1823790a512856b05ce5c295ff
bhare1987 <brandonlhare@gmail.com>
2016-02-26 14:03:26 -0500

    added conditional to hide delete for non active users

--- template.js
+++ template.js
@@ -1,11 +1,13 @@
 var templates = {};
  templates.messagetmpl=[
-   "<div class='messageContainer' data-id='<%=_id %>' data-user='<%= username %>'>",
+  "<div class='messageContainer' data-id='<%=_id %>' data-user='<%= username %>'>",
      "<span class='usenm'><%= username %></span>",
      "<span class='d8'><%= date %></span>",
-     "<span class='delete'>X</span>",
+     "<% if( obj.username === BrandTricks.config.activeUser) { %>",
+       "<span class='delete'>X</span>",
+     "<% } %>",
      "<div class='messagediv'><%= content %></div>",
-    "</div>",
+  "</div>",
  ].join("");
 
   templates.usertmpl= [
